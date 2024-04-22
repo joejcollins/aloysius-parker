dcc62a96ca34a3d42cf1e0df352631d9798dee08
Joe J Collins <joejcollins@users.noreply.github.com>
2024-04-22 12:27:49 +0000

    Confirm pre-commit

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 # Post create.
 make venv
 echo "make lint" > .git/hooks/pre-commit
-echo "make test" >> .git/hookss/pre-commit
+echo "make test" >> .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
