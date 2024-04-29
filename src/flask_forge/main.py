3112122aed17260f2500bc161ac789eb9606787e
Sentinel <vea.b256@gmail.com>
2024-04-29 09:41:30 +0000

    Add global error handler

--- src/flask_forge/main.py
+++ src/flask_forge/main.py
@@ -3,6 +3,7 @@
 from flask import Flask
 
 from flask_forge.config import blueprints, monitoring, open_api
+from flask_forge.error_handler import handle_error
 
 
 def create_app() -> Flask:
@@ -11,6 +12,7 @@ def create_app() -> Flask:
     open_api.configure_open_api(the_app)
     blueprints.configure_blueprints(the_app)
     monitoring.configure_monitoring(the_app)
+    the_app.register_error_handler(Exception, handle_error)
     return the_app
 
 
@@ -18,3 +20,5 @@ def run():
     """Run the Flask app."""
     app = create_app()
     app.run()
+
+run()
