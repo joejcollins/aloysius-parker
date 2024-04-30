8c2175bcfc777d9e747be5dfbe8eeac89701725e
Sentinel <vea.b256@gmail.com>
2024-04-30 14:49:04 +0000

    Error schema: remove redundant "code" field

--- src/flask_forge/error_handler.py
+++ src/flask_forge/error_handler.py
@@ -1,4 +1,6 @@
 """Error handling and customisation."""
+import http.client
+
 from flask import jsonify
 
 
@@ -6,13 +8,15 @@ def handle_error(error):
     """Handle errors and return a JSON response with the error message."""
     code = error.code if hasattr(error, "code") else 500
 
-    if isinstance(error, TypeError):
-        code = 400
+    # Yes 422 is more specific than 403, but most people do not know what 422 means
+    if error.code == http.client.UNPROCESSABLE_ENTITY:
+        code = http.client.FORBIDDEN
+    elif isinstance(error, TypeError):
+        code = http.client.FORBIDDEN
 
     return (
         jsonify(
             {
-                "code": code,
                 "error": str(error),
                 "type": error.__class__.__name__,
             }
