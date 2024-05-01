7797348ac1d6355c2cdc64eaa01a6e5241d32797
Sentinel <vea.b256@gmail.com>
2024-05-01 09:48:18 +0000

    Remove custom error handler

--- src/flask_forge/main.py
+++ src/flask_forge/main.py
@@ -1,10 +1,8 @@
 """Flask application for rapid deployment via Docker."""
-
 from flask import Flask
 
 from flask_forge.config import blueprints, monitoring, open_api
 from flask_forge.database.db import database
-from flask_forge.error_handler import handle_error
 
 
 def create_app() -> Flask:
@@ -13,7 +11,6 @@ def create_app() -> Flask:
     open_api.configure_open_api(the_app)
     blueprints.configure_blueprints(the_app)
     monitoring.configure_monitoring(the_app)
-    the_app.register_error_handler(Exception, handle_error)
     database.init_app(the_app)
     with the_app.app_context():
         database.create_all()
